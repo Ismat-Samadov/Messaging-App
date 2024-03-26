761c112a509ad2b10461ef21bd47b84184dbe4b6
Ismat-Samadov <ismetsemedov@gmail.com>
2024-03-27 02:02:30 +0400

    check

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 // server.js
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +12,11 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/messages', require('./routes/messages'));
 app.use('/api/users', require('./routes/users'));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 // Start server
 app.listen(PORT, () => {
