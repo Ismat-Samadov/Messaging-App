b65f57c256bd9f69705436919b5f6cd47145a05c
Ismat-Samadov <ismetsemedov@gmail.com>
2024-03-28 01:19:28 +0400

    check

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
