fbe78a03dcbbf56a820c5b5becdbe028ffe4f423
Ismat-Samadov <ismetsemedov@gmail.com>
2024-03-30 15:00:39 +0400

    check

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,5 +1,6 @@
-// backend/middleware/errorMiddleware.js
-exports.handleError = (err, req, res) => {
+// errorMiddleware.js
+
+exports.handleError = (err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).json({ message: 'Something went wrong!' });
+    res.status(500).send('Internal Server Error');
 };
