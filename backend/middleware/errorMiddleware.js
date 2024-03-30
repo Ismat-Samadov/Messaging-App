780304b703c248f5a3ba92509afe6790b7b0a523
Ismat-Samadov <ismetsemedov@gmail.com>
2024-03-30 22:36:24 +0400

    check

--- backend/middleware/errorMiddleware.js
+++
@@ -1,6 +0,0 @@
-// errorMiddleware.js
-
-exports.handleError = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Internal Server Error');
-};
--- backend/profile/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/profile/profileRoutes.js
-const express = require('express');
-const router = express.Router();
-const profileController = require('./profileController');
-
-// Update user profile
-router.put('/:userId', profileController.updateProfile);
-
-module.exports = router;
