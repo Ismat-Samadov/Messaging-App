35b8ba2bd9bff613d3581f866d68fd667c6d9537
Ismat-Samadov <ismetsemedov@gmail.com>
2024-04-01 01:19:43 +0400

    check

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,7 +9,7 @@ import Home from "./components/Home.tsx";
 import { Toaster } from "./components/ui/toaster.tsx";
 import ChatProvider from "./context/ChatProvider.tsx";
 
-const PROD = "https://messaging-app-jquv.onrender.com/api/v1";
+const PROD = "https://messaging-app-i1kr.onrender.com";
 // const DEV = "http://localhost:8000/api/v1";
 
 axios.defaults.baseURL = PROD;
