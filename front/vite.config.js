606246c833c9a57cde8fe50a24cfd3d2caa68329
Ismat-Samadov <ismetsemedov@gmail.com>
2024-04-07 00:00:12 +0400

    check

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,12 +1,34 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     globals: true,
+//     environment: "jsdom",
+//     setupFiles: "./tests/setup.js",
+//   },
+// });
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  },
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./tests/setup.js",
   },
 });
+
