6520339744a483152600e4c683baf56f517744cf
Ismat-Samadov <ismetsemedov@gmail.com>
2024-04-07 22:21:05 +0400

    check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { UserContextProvider } from './UserContext';
 import Routes from './Routes';
 
 function App() {
-  axios.defaults.baseURL = 'https://top-messagingapp-api.onrender.com';
+  axios.defaults.baseURL = 'https://messaging-app-i1kr.onrender.com';
   axios.defaults.withCredentials = true;
 
   return (
